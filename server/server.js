2c36efbc57ce9b3545b720531a7d554761207192
hilal aissani <hilalaissani@gmail.com>
2019-02-27 19:47:10 -0500

    2

--- server/server.js
+++ server/server.js
@@ -3,16 +3,22 @@ const server = express();
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const dotenv = require("dotenv");
+const path = require("path");
 
 dotenv.config();
 server.use(cors());
 server.use(bodyParser.json());
 
-server.get("/", (req, res) => {
+const staticFiles = express.static(path.join(__dirname, "../build"));
+
+https: server.get("/", (req, res) => {
   res.send(" === api is running === ");
 });
+
 const nodemailerRoute = require("./nodemailer");
 server.use("/message", nodemailerRoute);
+
+server.use("*", staticFiles);
 
 server.listen(4000, () => {
   console.log("== server is running on port 4000 ====");
