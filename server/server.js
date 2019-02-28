521ad8f5ac576e638d8929fc65cbd08b3c563911
hilal aissani <hilalaissani@gmail.com>
2019-02-28 17:58:10 -0500

    3

--- server/server.js
+++ server/server.js
@@ -4,12 +4,29 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 const dotenv = require("dotenv");
 const path = require("path");
+const morgan = require("morgan");
 
 dotenv.config();
 server.use(cors());
 server.use(bodyParser.json());
 
+const port = process.env.PORT || 4000;
 const staticFiles = express.static(path.join(__dirname, "../build"));
+
+server.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET,HEAD,OPTIONS,POST,PUT,DELETE"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers"
+  );
+  res.setHeader("Cache-Control", "no-cache");
+  next();
+});
 
 https: server.get("/", (req, res) => {
   res.send(" === api is running === ");
@@ -20,6 +37,6 @@ server.use("/message", nodemailerRoute);
 
 server.use("*", staticFiles);
 
-server.listen(4000, () => {
+server.listen(port, () => {
   console.log("== server is running on port 4000 ====");
 });
